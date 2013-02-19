f50b5a8141b4d99e17d28314d6f937188f39e6dd
Mattias Linnap <mattias@linnap.com>
2013-02-19 20:05:03 +0000

    Fix airplane mode logging, it was flipped.

--- library/src/org/yousense/shared/receivers/AirplaneReceiver.java
+++ library/src/org/yousense/shared/receivers/AirplaneReceiver.java
@@ -3,16 +3,22 @@ package org.yousense.shared.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import org.yousense.common.Pretty;
 import org.yousense.eventlog.EventLog;
+import org.yousense.shared.Config;
 
 public class AirplaneReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (intent.getBooleanExtra("state", false))
-            EventLog.append("device.airplane.off", null);
-        else
-            EventLog.append("device.airplane.on", null);
+        if (intent.hasExtra("state")) {
+            boolean airplane = intent.getBooleanExtra("state", false);
+            if (airplane)
+                EventLog.append("device.airplane.on", null);
+            else
+                EventLog.append("device.airplane.off", null);
+        }
 	}
 
 }
