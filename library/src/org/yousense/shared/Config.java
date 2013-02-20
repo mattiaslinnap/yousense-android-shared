51bf1892522cd5340d05e4381c4d55ae8553d6e9
Mattias Linnap <mattias@linnap.com>
2013-02-20 18:27:38 +0000

    Don't open the app on persistent service notification click.

--- library/src/org/yousense/shared/Config.java
+++ library/src/org/yousense/shared/Config.java
@@ -3,5 +3,5 @@ package org.yousense.shared;
 public class Config {
     public static final String TAG = "yousense-shared";
     public static final long HEARTBEAT_INTERVAL = 5*60*1000;
-    public static final long PAUSE_INTERVAL = 10*60*1000;
+    public static final long PAUSE_INTERVAL = 24*3600*1000;
 }
